bc41956c4c3f9268e718fb5bbacd3bdb85ccbb3d
Rael Max <contato@raelmax.com>
2013-06-21 10:52:36 -0300

    override default queryset to get image

--- opps/images/views/__init__.py
+++ opps/images/views/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from django.contrib.sites.models import get_current_site
+from django.utils import timezone
+
 from opps.articles.views.generic import OppsDetail
 
 from ..models import Image
@@ -8,14 +11,13 @@ from ..models import Image
 class ImageDetail(OppsDetail):
     model = Image
     type = 'images'
+    template_name_suffix = '_images'
 
     @property
     def queryset(self):
         self.site = get_current_site(self.request).domain
-        self.long_slug = self.kwargs['tag']
-        self.article = self.model.objects.filter(
-            site_domain=self.site,
-            tags__slug=self.long_slug,
+        self.long_slug = self.kwargs.get('slug')
+        self.image = self.model.objects.get(
+            slug=self.kwargs.get('slug'),
             date_available__lte=timezone.now(),
-            published=True).all()
-        return self.article
+            published=True)
